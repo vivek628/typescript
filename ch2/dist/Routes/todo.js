700da23d888ef275ae9888998d8a039e18d3e469
vivek628 <siloriv2@gmail.com>
2024-08-20 07:21:41 +0530

    typescriptshowdown

--- ch2/dist/Routes/todo.js
+++ ch2/dist/Routes/todo.js
@@ -7,26 +7,31 @@ router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todo });
 });
 router.post('/todo', (req, res, next) => {
+    const body = req.body;
     const newTodo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     };
     todo.push(newTodo);
     return res.status(201).json({ message: "ok bro" });
 });
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const param = req.params;
+    const body = req.body;
+    const tid = param.todoId;
     const todoindex = todo.findIndex((todoitem) => {
         todoitem.id === tid;
     });
     if (todoindex >= 0) {
-        todo[todoindex] = { id: todo[todoindex].id, text: req.body.text };
+        todo[todoindex] = { id: todo[todoindex].id, text: body.text };
         res.status(201).json({ message: "task done" });
     }
     res.status(404).json({ message: "could not find id " });
 });
-router.post('/delete/:totoId', (req, res, next) => {
-    const idtodelete = req.params.totoId;
+router.post('/delete/:todoId', (req, res, next) => {
+    const body = req.body;
+    const param = req.params;
+    const idtodelete = param.todoId;
     todo.filter(todoitem => todoitem.id != idtodelete);
     res.status(200).json({ message: "deleted" });
 });
--- ch2/src/Routes/todo.ts
+++ ch2/src/Routes/todo.ts
@@ -1,32 +1,39 @@
 import { Router } from "express";
 import { Todo } from "../models/todo";
 let todo:Todo[]=[]
+type RequestParams={todoId:'string'}
+type Requestbody={text:'string'}
 const router= Router()
  router.get('/',(req,res,next)=>{
     res.status(200).json({todos:todo})
  })
  router.post('/todo',(req,res,next)=>{
+   const body=req.body as Requestbody
     const newTodo:Todo={
         id:new Date().toISOString(),
-        text:req.body.text
+        text:body.text
     }
     todo.push(newTodo)
     return res.status(201).json({message:"ok bro"})
  })
  router.put('/todo/:todoId',(req,res,next)=>{
-    const tid=req.params.todoId
+   const param= req.params as RequestParams
+   const body=req.body as Requestbody
+    const tid=param.todoId
     const todoindex=todo.findIndex((todoitem)=>{
         todoitem.id===tid
     })
     if(todoindex>=0)
     {
-        todo[todoindex]={ id:todo[todoindex].id,text:req.body.text}
+        todo[todoindex]={ id:todo[todoindex].id,text:body.text}
         res.status(201).json({message:"task done"})
     }
     res.status(404).json({message:"could not find id "})
  })
- router.post('/delete/:totoId',(req,res,next)=>{
-    const idtodelete=req.params.totoId
+ router.post('/delete/:todoId',(req,res,next)=>{
+   const body= req.body as Requestbody
+   const param= req.params as RequestParams
+    const idtodelete=param.todoId
     todo.filter(todoitem=>todoitem.id!=idtodelete)
     res.status(200).json({message:"deleted"})
 
